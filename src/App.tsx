849c2043e4af75313ffcee91fe27e9693c30862b
ismaelp96 <ismael6991@live.com>
2025-11-10 14:30:08 -0300

    feat: Componente InputText

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import Button from "./components/button";
-import ButtonIcon from "./components/button-icon";
-import ChevronLeftIcon from "./assets/icons/chevron-left.svg?react";
-import ChevronRightIcon from "./assets/icons/chevron-right.svg?react";
-import Badge from "./components/badge";
-import Alert from "./components/alert";
-import Divider from "./components/divider";
+import Button from './components/button';
+import ButtonIcon from './components/button-icon';
+import ChevronLeftIcon from './assets/icons/chevron-left.svg?react';
+import ChevronRightIcon from './assets/icons/chevron-right.svg?react';
+import SearchIcon from './assets/icons/search.svg?react';
+import Badge from './components/badge';
+import Alert from './components/alert';
+import Divider from './components/divider';
+import InputText from './components/input-text';
 
 export default function App() {
 	return (
-		<div className="grid gap-7 p-6">
-			<div className="flex gap-3">
+		<div className='grid gap-7 p-6'>
+			<div className='flex gap-3'>
 				<Button>Button</Button>
-				<Button variant="secondary">Button</Button>
+				<Button variant='secondary'>Button</Button>
 				<Button disabled>Button</Button>
 				<Button handling>Loading</Button>
 				<Button icon={ChevronRightIcon}>Próxima Imagem</Button>
-				<Button variant="ghost" size="sm">
+				<Button variant='ghost' size='sm'>
 					Button
 				</Button>
-				<Button variant="primary" size="sm">
+				<Button variant='primary' size='sm'>
 					Button
 				</Button>
 			</div>
 
-			<div className="flex gap-3">
+			<div className='flex gap-3'>
 				<ButtonIcon icon={ChevronLeftIcon} />
-				<ButtonIcon icon={ChevronRightIcon} variant="secondary" />
+				<ButtonIcon icon={ChevronRightIcon} variant='secondary' />
 			</div>
 
-			<div className="flex gap-3">
+			<div className='flex gap-3'>
 				<Badge>Todos</Badge>
 				<Badge>Natureza</Badge>
 				<Badge>Viagem</Badge>
@@ -47,6 +49,9 @@ export default function App() {
 
 			<div>
 				<Divider />
+			</div>
+			<div>
+				<InputText placeholder='Buscar fotos' icon={SearchIcon} />
 			</div>
 		</div>
 	);
