0f7eba257d20e54a0693b206f8edabe01d1d9456
ismaelp96 <ismael6991@live.com>
2025-11-28 11:37:51 -0300

    feat: Mock da página de detalhes da foto - imagem e botão de excluir

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,4 +1,6 @@
+import Button from '../components/button';
 import Container from '../components/container';
+import ImagePreview from '../components/image-preview';
 import Skeleton from '../components/skeleton';
 import Text from '../components/text';
 import PhotosNavigator from '../contexts/photos/components/photos-navigator';
@@ -11,12 +13,29 @@ export function PagePhotoDetails() {
 		<Container>
 			<header className='flex items-center justify-between gap-8 mb-8'>
 				{isLoadingPhoto ? (
-					<Text variant='heading-large'>{photo.title}</Text>
+					<Text variant='heading-large'>{photo?.title}</Text>
 				) : (
 					<Skeleton className='w-48 h8' />
 				)}
 				<PhotosNavigator />
 			</header>
+			<div className='grid grid-cols-[21rem] gap-24'>
+				{!isLoadingPhoto ? (
+					<div className='space-y-3'>
+						<ImagePreview
+							src={`images/${photo?.imageId}`}
+							title={photo?.title}
+							imageClassName='h-[21rem]'
+						/>
+						<Button variant='destructive'>Excluir</Button>
+					</div>
+				) : (
+					<div className='space-y-3'>
+						<Skeleton className='h-[21rem]' />
+						<Skeleton className='w-20 h-10' />
+					</div>
+				)}
+			</div>
 		</Container>
 	);
 }
